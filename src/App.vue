339fb37c1030902ccbc71840abc7ad54f7bca2be
Gaspar RIVOIRE-GAIAO <gaspar.rivoire-gaiao@groupama.com>
2025-02-12 10:27:01 +0100

    Components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <Layout>
+    <template #header> En tête </template>
+    <template #aside>aside</template>
+    <template #main>main</template>
+    <template #footer>footer</template>
+  </Layout>
   <form action="" @submit.prevent="ajouterTache(newTache)">
     <input type="text" placeholder="Ajouter une tache" v-model="newTache" />
     <button :disabled="newTache.length === 0">Ajouter</button>
@@ -6,7 +12,7 @@
 
   <ul v-if="taches.length !== 0">
     <li
-      v-for="tache in sortedTodos()"
+      v-for="tache in sortedTodos"
       :key="tache.date"
       :class="{ completed: tache.isDone }"
     >
@@ -17,14 +23,25 @@
     </li>
   </ul>
   <h1 v-else>Pas de taches</h1>
-  <label>
-    <input type="checkbox" v-model="isHidden" />
-    Masquer les taches terminées
-  </label>
+  <Checkbox
+    label="Masquer les composants terminés"
+    @check="console.log('checked')"
+    @uncheck="console.log('unchecked')"
+    v-model="isHidden"
+  />
+
+  <Checkbox
+    label="Bonjour"
+    @check="console.log('checked')"
+    @uncheck="console.log('unchecked')"
+  />
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import Checkbox from "/src/Checkbox.vue";
+import Button from "/src/Button.vue";
+import Layout from "./Layout.vue";
 
 const isHidden = ref(false);
 
@@ -46,15 +63,15 @@ const ajouterTache = (tache) => {
   });
 };
 
-const sortedTodos = () => {
-  const sortedTodos = taches.value.sort((a, b) =>
+const sortedTodos = computed(() => {
+  const sorted = [...taches.value].sort((a, b) =>
     a.isDone > b.isDone ? 1 : -1
   );
-  if (isHidden.value === true) {
-    return sortedTodos.filter((tache) => tache.isDone === false);
+  if (isHidden.value) {
+    return sorted.filter((tache) => !tache.isDone);
   }
-  return sortedTodos;
-};
+  return sorted;
+});
 </script>
 
 <style>
---
+++ src/Checkbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <label>
+    <input type="checkbox" @change="onChange" v-model="model" />
+    {{ label }}
+  </label>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: String,
+});
+
+const model = defineModel();
+
+const onChange = (event) => {
+  if (event.currentTarget.checked) {
+    emits("check", event.currentTarget);
+  } else {
+    emits("uncheck", event.currentTarget);
+  }
+};
+
+const emits = defineEmits(["check", "uncheck"]);
+
+console.log(props.label);
+</script>
